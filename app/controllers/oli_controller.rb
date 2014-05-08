18cd85ca4e1e705f55ad706710b753b0a9190e17
jh <jh@jhs-MacBook-Pro.local>
2014-05-08 10:43:57 -0400

    added subsribers

--- app/controllers/oli_controller.rb
+++ app/controllers/oli_controller.rb
@@ -1,4 +1,24 @@
+require 'aweber'
+
 class OliController < ApplicationController
+
+  CONSUMER_KEY = "AkB5AxUg1suXLOEDRs3XWILU" 
+  CONSUMER_SECRET = "VD02JR26eEmvv0xfaLriafujPHaruAtHt4LXISM1" 
+  ACCESS_TOKEN = "AgliSsKHr64scyPPJe0eIwvu"
+  ACCESS_TOKEN_SECRET = "SsqIjkwe4KCCGFr4l0BvVrviZXfJdnXn33Qs1R45"
+
   def landing
+    
+  end
+
+  def subscribe
+    oauth = AWeber::OAuth.new(CONSUMER_KEY, CONSUMER_SECRET)
+    oauth.authorize_with_access(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    aweber = AWeber::Base.new(oauth)
+    new_subscriber = {}
+    new_subscriber["email"] = params[:data]
+    new_subscriber["name"] = "No name"
+    aweber.account.lists.find_by_name("test-api").subscribers.create(new_subscriber)
+    render :landing
   end
 end
