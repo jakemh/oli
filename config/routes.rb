25567a5d65c91c3af285eaaccf0407ca99c4a251
jh <jh@jhs-MacBook-Pro.local>
2014-05-08 18:25:59 -0400

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'oli/landing'
-  match "/oli/landing" => "oli#subscribe", via: :post
+  match "/landing" => "oli#landing", via: :get
+  match "/landing" => "oli#subscribe", via: :post
+  root to: 'oli#landing'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
