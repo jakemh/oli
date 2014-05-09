24b5555c119d1f62099122add45ac1ee54f195e0
jh <jh@jhs-MacBook-Pro.local>
2014-05-09 14:55:33 -0400

    added loader

---
+++ app/assets/javascripts/xoli.js
@@ -0,0 +1,24 @@
+$(document ).ready(function() {
+  var laddaLoadingButton = Ladda.create( document.querySelector( '.ladda-button' ) );
+
+      $("#oli-form").submit(function() {
+        laddaLoadingButton.start();
+        $.ajax({
+        url: '/landing',
+        type: 'post',
+        async: true,
+        data: $("#oli-form").serialize(),
+        dataType: 'script',
+        beforeSend: function (xhr) {
+          xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
+        }
+        }).done(function() {
+
+        }).success(function(){
+          laddaLoadingButton.stop();
+
+        });
+      return false;
+      })
+
+  });
