fa32a28cd4d84c70d5474dd2040456e4887b93f7
jh <jh@jhs-MacBook-Pro.local>
2014-05-13 12:06:40 -0400

    added loader

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :sass
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
