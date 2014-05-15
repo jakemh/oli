e08bc040a883f96799ac2e69ba9ae0f56b67498d
jh <jh@jhs-MacBook-Pro.local>
2014-05-14 23:47:10 -0400

    set prod db to pg

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,6 @@ set :default_stage, "staging"
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '~/www/oli'
-set :deploy_via, :remote_cache
 
 # Default value for :scm is :git
 # set :scm, :git
