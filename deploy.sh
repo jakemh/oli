b285f3eff7e446fa6e14c0f6f4dbc14e00bc35be
jh <jh@jhs-MacBook-Pro.local>
2014-05-16 18:36:54 -0400

    pass

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set pass "oli2014"
 
 git add .
 git commit -m "$1"
 git push origin master
 cap production deploy
-expect "Please enter Server password ():"
-send "oli2014"
+expect "Please enter Server password (): "
+send "$pass"
