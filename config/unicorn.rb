3a712dfb8c8fd34ecafe2b87690426c7a44cf250
jh <jh@jhs-MacBook-Pro.local>
2014-05-16 14:35:13 -0400

    change secret_key_base temporarily

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -12,6 +12,6 @@ timeout 30
 # reference the capistrano "current" symlink
 before_exec do |_|
   ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
-  ENV["SECRET_KEY_BASE"] = %x[rake secret].to_s
+  ENV["SECRET_KEY_BASE"] = '76015d74859449d720617ad69330b4b769d1b5d14f213c14c4c63b8ce2a8e26c659a16792bf81fb0f1e0e4a1a5289140165d5f040157659ece6ff288a5601d77'
 end
 
