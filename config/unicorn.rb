5f2f5b7be933275780682172e695686fa4b876d2
jh <jh@jhs-MacBook-Pro.local>
2014-05-15 21:18:23 -0400

    capistrano changes

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 root = "/home/oli/www/shared"
-working_directory "/home/oli/www/current"
+working_directory = "/home/oli/www/current"
 pid "#{working_directory}/tmp/pids/unicorn.pid"
 stderr_path "#{working_directory}/log/unicorn.log"
 stdout_path "#{working_directory}/log/unicorn.log"
