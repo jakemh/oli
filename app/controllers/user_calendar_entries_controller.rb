94a7426c51b2d52c5ee2b8447e35548d187c3f10
jh <jh@jhs-MacBook-Pro.local>
2014-06-26 20:02:51 -0400

    fixed bugs with level 3

--- app/controllers/user_calendar_entries_controller.rb
+++ app/controllers/user_calendar_entries_controller.rb
@@ -1,14 +1,14 @@
 class UserCalendarEntriesController < ApplicationController
   
   def list
-    render :json => UserCalendarEntry.find(params[:ids]), :root => "userCalendarEntries"
+    render :json => UserCalendarEntry.where(:user => current_user, :id => params[:ids]), :root => "userCalendarEntries"
   end
 
   def update
   end
 
   def show
-    render :json => UserCalendarEntry.find(params[:id]), :root => "userCalendarEntry"
+    render :json => UserCalendarEntry.where(:user => current_user, :id => params[:id]), :root => "userCalendarEntry"
   end
 
   def create
